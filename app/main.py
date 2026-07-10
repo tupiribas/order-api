11bef188cc2077b3e7bf16b97aada166fe753120
tupiribas <tupiguedes@outlook.com>
2026-07-09 21:41:21 -0300

    feat: add create order route and validation schema

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api.routers import router
 
 app = FastAPI(title="Order API")
 
@@ -9,3 +10,8 @@ def health_check():
     Base utilizada para o código: https://fastapi.tiangolo.com/pt/#installation
     '''
     return {"title": app.title, "status": "online"}
+
+
+# Base de conhecimento: https://fastapi.tiangolo.com/reference/apirouter/?h=
+# include_router#fastapi.APIRouter.include_router
+app.include_router(router)
