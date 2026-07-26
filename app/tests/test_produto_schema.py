35b135b42ae71a451a9a3ec91a40c815778455d4
tupiribas <tupiguedes@outlook.com>
2026-07-26 15:57:20 -0300

    feat: update function test - test_should_create_produto_weth_validade_data()

--- app/tests/test_produto_schema.py
+++ app/tests/test_produto_schema.py
@@ -18,24 +18,32 @@ def test_should_create_produto_weth_validade_data():
 
 
 def test_should_reject_produto_with_invalid_name_type():
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError) as exc_info:
         # Arrange:
         dados = {
-            "nome": 3213,
+            "nome": ["Produto", "Teste"],
             "preco_base": 25.9
         }
+        # Act
         ProdutoCreate(**dados)
+
+    erros = exc_info.value.errors()
+    assert erros[0]["loc"] == ("nome",)
+    assert "string_type" in erros[0]["type"]
 
 
 def test_should_reject_produto_with_price_base_value_type_not_valid():
     with pytest.raises(ValidationError) as exc_info:
+        # Arrange:
         dados = {
             "nome": "Validador",
             "preco_base": None
         }
+        # Act
         ProdutoCreate(**dados)  # type: ignore
 
-    # Você pode inspecionar os detalhes do erro
+    # Inspecionando os detalhes do erro
+    # Assert
     erros = exc_info.value.errors()
     assert erros[0]["loc"] == ("preco_base",)
     assert "float_type" in erros[0]["type"]
@@ -47,9 +55,11 @@ def test_should_reject_produto_with_price_base_float_parsing():
             "nome": "Validador",
             "preco_base": "Validador"
         }
+        # Act
         ProdutoCreate(**dados)  # type: ignore
 
-    # Você pode inspecionar os detalhes do erro
+    # Inspecionando os detalhes do erro
+    # Assert
     erros = exc_info.value.errors()
     assert erros[0]["loc"] == ("preco_base",)
     assert "float_parsing" in erros[0]["type"]
